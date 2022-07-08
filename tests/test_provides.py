3d5136718fc710d53a38a28973d4255409ead6af
Adam Dyess <adam.dyess@canonical.com>
2022-07-08 11:33:25 -0500

    blacken, and automated github actions testing (#35)
    * blacken, and automated github actions testing
    
    * set flake line-length to 88

--- tests/test_provides.py
+++ tests/test_provides.py
@@ -5,9 +5,6 @@ import provides
 def test_set_default_cni():
     provider = provides.KubeControlProvider()
     provider.relations = [MagicMock(), MagicMock()]
-    provider.set_default_cni('test')
+    provider.set_default_cni("test")
     for relation in provider.relations:
-        relation.to_publish.__setitem__.assert_called_once_with(
-            'default-cni',
-            'test'
-        )
+        relation.to_publish.__setitem__.assert_called_once_with("default-cni", "test")
--- tests/test_requires.py
+++ tests/test_requires.py
@@ -3,5 +3,5 @@ import requires
 
 def test_get_default_cni():
     requirer = requires.KubeControlRequirer()
-    requirer.all_joined_units.received = {'default-cni': 'test'}
-    assert requirer.get_default_cni() == 'test'
+    requirer.all_joined_units.received = {"default-cni": "test"}
+    assert requirer.get_default_cni() == "test"
